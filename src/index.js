270e74dd8c973398b1690255648c47572c3da500
Adrian Camacho <acr77frb@gmail.com>
2025-06-13 21:51:47 -0600

    Enabled  CORS.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import dotenv from "dotenv";
 import express from "express";
 import router from "./router.js";
 import connectDB from "./db.js"
+import cors from "cors";
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/", router);
 dotenv.config();
